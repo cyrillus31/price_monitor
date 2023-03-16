4ddce34ca6a49eaeae6d1f2de03bf801b2710ff4
Kirill F <kirill.olegovich31@gmail.com>
2023-03-16 09:36:48 +0300

    adding telegram api support

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import requests, bs4, re, json, time
+import requests, bs4, re, json, time, os
 import sqlite3
 
 pattern = r"(?<=var offers = ){.+}(?=;)"
@@ -16,26 +16,49 @@ class Page:
         search = response.text
         myjson_string = re.findall(pattern, search)[0]
         my_json = json.loads(myjson_string)
+        # print(my_json)
         
         connection = sqlite3.connect("data.db")
         cursor = connection.cursor()
-        qselect = "SELECT * FROM macbook WHERE name=?"
-        qinsert = "INSERT INTO macbook VALUES (?, ?)"
-        qupdate = "UPDATE macbook SET price=? WHERE name=?"
+        qselect = "SELECT name, price, discount, quantity FROM macbook WHERE name=?"
+        qinsert = "INSERT INTO macbook VALUES (?, ?, ?, ?)"
+        qupdate = "UPDATE macbook SET price=?, discount=? WHERE name=?"
         for key in my_json:
             # print(key)
             name = my_json[key]["NAME"]
             if name[:7] != "Ноутбук":
                 continue
             price = int(my_json[key]["PRICE"])
-            if cursor.execute(qselect, (name,)).fetchone():
-                cursor.execute(qupdate, (price, name))
+            quantity = int(my_json[key]["QUANTITY"])
+            discount = int(my_json[key]["DISCOUNT"])
+            fetch = cursor.execute(qselect, (name,)).fetchone()
+            if fetch[1] != price or fetch[2] != discount or fetch[3] != quantity:
+                cursor.execute(qupdate, (price, discount, name))
+                self.is_updated = True
             else:
-                cursor.execute(qinsert, (name, price))
+                cursor.execute(qinsert, (name, price, discount, quantity))
         
         connection.commit()
         connection.close()
+    
+    def get_table():
+        connection = sqlite3.connect("data.db")
+        cursor = connection.cursor()
 
+        get_table = "SELECT name, price, discount, quantity FROM macbook"
+        all_rows = cursor.execute(get_table).fetchall()
+
+
+
+TOKEN = os.getenv("PRICE_MONITOR_TELEBOT_TOKEN")
+CHAT_ID = 617762423
+
+def sending_updates(token, is_updated: bool, chat_id) -> None:
+    if is_updated:
+        url = "https://api.telegram.org/bot" + token
+        method = "/getUpdates"
+        r = requests.get(url+method)
+        print(r.content)
 
 
 
@@ -45,6 +68,9 @@ if __name__ == "__main__":
     my_search = Page(url)
     while True:
         my_search.update_database()
-        time.sleep(1800)
+        # time.sleep(1800)
+        # time.sleep(3)
+        sending_updates(TOKEN, True, CHAT_ID)
+        break
     
 
