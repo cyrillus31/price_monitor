bd82d6612f9b265ad2583fbb19931336ced5332f
cyrillus31 <kirill.olegovich31@gmail.com>
2023-03-17 18:03:25 +0300

    first functional version

--- db.py
+++ db.py
@@ -4,6 +4,7 @@ connection = sqlite3.connect("data.db")
 cursor = connection.cursor()
 
 create_table = "CREATE TABLE IF NOT EXISTS macbook (name TEXT, price INTEGER, quantity INTEGER, discount INTEGER)"
+cursor.execute(create_table)
 
 connection.commit()
 connection.close()
